136f1a9b11fa415d7e9b22e66a0d9e86b0f3d78f
nvliep <liep.nguyen@orientsoftware.net>
2014-10-29 17:58:24 +0700

    initial commit

--- Planru.Core.WebAPI/Routes/IRouteProvider.cs
+++ Planru.Core.WebAPI/Routes/IRouteProvider.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Routing;
 
 namespace Planru.Core.WebAPI.Routes
 {
     public interface IRouteProvider
     {
         void RegisterRoutes(RouteCollection routes);
+
         int Priority { get; }
     }
 }
