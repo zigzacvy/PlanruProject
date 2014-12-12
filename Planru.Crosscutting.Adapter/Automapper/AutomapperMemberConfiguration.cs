aa352dd78f60ab53e1aa313cc454af318272e152
Liep Nguyen <liepnguyen@live.com>
2014-12-12 22:18:31 +0700

    Initial commit

---
+++ Planru.Crosscutting.Adapter/Automapper/AutomapperMemberConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using AutoMapper;
+
+namespace Planru.Crosscutting.Adapter.Automapper
+{
+    public class AutomapperMemberConfiguration<TSource> : IMemberConfiguration<TSource>
+    {
+        private Expression<Func<TSource, object>> _sourceMember;
+
+        public void MapFrom(Expression<Func<TSource, object>> sourceMember)
+        {
+            _sourceMember = sourceMember;
+        }
+
+        public Expression<Func<TSource, object>> SourceMember { get { return _sourceMember; } }
+    }
+}
