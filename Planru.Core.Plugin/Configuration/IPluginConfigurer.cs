abdb23c3692f4fa811a1fe54dc3196649214af3b
Liep Nguyen <liepnguyen@live.com>
2014-12-13 15:08:06 +0700

    Initial commit

---
+++ Planru.Core.Plugin/Configuration/IPluginConfigurer.cs
@@ -0,0 +1,14 @@
+﻿using Planru.Crosscutting.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planru.Core.Plugin.Configuration
+{
+    public interface IPluginConfigurer 
+    {
+        void ConfigureMappings(ITypeAdapter typeAdapter);
+    }
+}
---
+++ Planru.Plugins.Directory/Mappings.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.Serialization;
+using Planru.Core.Domain;
+using Planru.Crosscutting.Adapter;
+using Planru.Plugins.Directory.Domain;
+using Planru.Plugins.Directory.Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planru.Plugins.Directory
+{
+    public class Mappings : AdapterProfile
+    {
+        public override void Configure()
+        {
+            CreateMap<User, UserDTO>();
+
+            BsonClassMap.RegisterClassMap<EntityAudit<Guid>>(cm =>
+            {
+                cm.AutoMap();
+                cm.GetMemberMap(c => c.CreatedDateTime).SetElementName("created_date_time");
+                cm.GetMemberMap(c => c.UpdatedDateTime).SetElementName("updated_date_time");
+            });
+
+            BsonClassMap.RegisterClassMap<User>(cm =>
+            {
+                cm.AutoMap();
+                cm.GetMemberMap(c => c.FirstName).SetElementName("first_name");
+                cm.GetMemberMap(c => c.LastName).SetElementName("last_name");
+            });
+        }
+    }
+}
